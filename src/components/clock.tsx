3f99bd4fd8b0d107d8c872cb62f6b2f3c37cd7fd
Buntin-Synthia <aokitakumi72@gmail.com>
2023-04-20 05:02:32 +0900

    clock block

--- src/components/clock.tsx
+++ src/components/clock.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import '@styles/clock.css';
 
 const Clock = () => {
     const [time, setTime] = useState(new Date());
@@ -9,13 +10,32 @@ const Clock = () => {
     }, []);
 
     return (
-        <div>
-            <h1>Clock:</h1>
-            <div className="clock">
-                <p>Date: {time.toDateString()}</p>
-                <p>Time: {time.toLocaleString()}</p>
-            </div>
-        </div>
+        <>
+            <a
+                href="#" //center
+                className="block max-w-sm rounded-lg border border-gray-200 bg-white p-6 text-center shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+            >
+                <h2 className="text-xl font-bold text-gray-800 dark:text-gray-200">
+                    NOW
+                </h2>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                    Date:{' '}
+                    {`${time.getFullYear()}-${
+                        time.getMonth() + 1
+                    }-${time.getDate()}(${
+                        ['日', '月', '火', '水', '木', '金', '土'][
+                            time.getDay()
+                        ]
+                    })${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`.replace(
+                        /\n|\r/g,
+                        ''
+                    )}
+                </p>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                    Time: {time.toLocaleString()}
+                </p>
+            </a>
+        </>
     );
 };
 
